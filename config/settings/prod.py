5a1c11704df2b4f95ca30530dd78210ce82f86a0
도화 <sismis123@korea.ac.kr>
2020-09-19 20:22:13 +0900

    staticroot

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 
 ALLOWED_HOSTS = ['3.34.67.108', 'infobeauty.kr']
 
-STATIC_ROOT = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = BASE_DIR / 'static/'
 
 STATICFILES_DIRS = []
 
